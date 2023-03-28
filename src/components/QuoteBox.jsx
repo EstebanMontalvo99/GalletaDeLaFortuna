e1811563340e4d96148e47093f632366a2ac43e3
EstebanMontalvo99 <estebanmontalvo99@hotmail.com>
2023-03-28 17:19:57 -0500

    clip-path

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 const QuoteBox = ({ quote }) => {
   return (
     <div className="box">
-      <p className="box__quote">{quote.quote}</p>
-      <h2 className="box__author">{quote.author}</h2>
-      <i className="bx bxs-quote-alt-right"></i>
+      <div className="miniBox">
+        <p className="box__quote">{quote.phrase}</p>
+        <h2 className="box__author">{quote.author}</h2>
+        <i className="bx bxs-quote-alt-right"></i>
+      </div>
     </div>
   );
 };
